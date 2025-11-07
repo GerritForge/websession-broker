19a5d1c29df4f956f5925a684bb1cd6a64c43b83
Daniele Sassoli <danielesassoli@gmail.com>
2025-11-07 10:25:49 +0000

    Update base package to com.gerritforge and add BSL license header
    Change-Id: If53080509b74814b943b7d270b071a6e9ed63466

--- src/main/java/com/gerritforge/gerrit/plugins/websession/broker/BrokerBasedWebSession.java
+++ src/main/java/com/gerritforge/gerrit/plugins/websession/broker/BrokerBasedWebSession.java
@@ -1,10 +1,7 @@
-// Copyright (C) 2019 The Android Open Source Project
+// Copyright (C) 2025 GerritForge, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the BSL 1.1 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,7 +9,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.googlesource.gerrit.plugins.websession.broker;
+package com.gerritforge.gerrit.plugins.websession.broker;
 
 import com.google.gerrit.extensions.annotations.RootRelative;
 import com.google.gerrit.extensions.events.LifecycleListener;
@@ -33,8 +30,8 @@ import com.google.inject.Scopes;
 import com.google.inject.internal.UniqueAnnotations;
 import com.google.inject.servlet.RequestScoped;
 import com.google.inject.servlet.ServletScopes;
-import com.googlesource.gerrit.plugins.websession.broker.log.Log4jWebSessionLogger;
-import com.googlesource.gerrit.plugins.websession.broker.log.WebSessionLogger;
+import com.gerritforge.gerrit.plugins.websession.broker.log.Log4jWebSessionLogger;
+import com.gerritforge.gerrit.plugins.websession.broker.log.WebSessionLogger;
 import java.lang.annotation.Annotation;
 import java.util.concurrent.ExecutorService;
 import javax.servlet.http.HttpServletRequest;
