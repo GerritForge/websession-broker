84e3957a05bc23e9891cdbd8d7edcd567667ae24
Daniele Sassoli <danielesassoli@gmail.com>
2026-09-07 14:56:40 +0100

    Connect to BrokerApi only when concrete implementation is loaded
    With Ia3d99d8af6, events-broker exposes a listener that tells us when a
    new broker is bound.
    
    Subscribe to events-broker BrokerApiPluginListener so that when can
    subscribe to broker implementations as soon as they're bound.
    
    Bug: Issue 556516033
    Change-Id: Ifff9539de419b5c7667d09463ae7b334ccdf6d12

---
+++ src/main/java/com/gerritforge/gerrit/plugins/websession/broker/WebSessionBrokerApiLoadedListener.java
@@ -0,0 +1,78 @@
+// Copyright (C) 2026 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.websession.broker;
+
+import com.gerritforge.gerrit.eventbroker.AckAwareConsumer;
+import com.gerritforge.gerrit.eventbroker.BrokerApi;
+import com.gerritforge.gerrit.eventbroker.BrokerApiPluginListener;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.extensions.registration.DynamicItem;
+import com.google.gerrit.server.events.Event;
+import com.google.gerrit.server.plugins.StartPluginListener;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.google.inject.internal.UniqueAnnotations;
+
+/** Subscribes to a topic as soon as a broker plugin has bound its {@link BrokerApi}. */
+@Singleton
+public class WebSessionBrokerApiLoadedListener implements BrokerApiPluginListener {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final DynamicItem<BrokerApi> brokerApi;
+
+  private String topic;
+  private AckAwareConsumer<Event> consumer;
+  private boolean replayAllEvents;
+
+  @Inject
+  WebSessionBrokerApiLoadedListener(DynamicItem<BrokerApi> brokerApi) {
+    this.brokerApi = brokerApi;
+  }
+
+  @Override
+  public DynamicItem<BrokerApi> brokerApiDynamicItem() {
+    return brokerApi;
+  }
+
+  synchronized void subscribe(String topic, AckAwareConsumer<Event> consumer, boolean replayAll) {
+    this.topic = topic;
+    this.consumer = consumer;
+    this.replayAllEvents = replayAll;
+    if (isBrokerApiStarted()) {
+      onBrokerApiStarted();
+    } else {
+      logger.atInfo().log(
+          "No broker plugin started, waiting before subscribing to topic %s", topic);
+    }
+  }
+
+  @Override
+  public synchronized void onBrokerApiStarted() {
+    logger.atInfo().log(
+        "Subscribing to topic %s on broker plugin %s", topic, brokerApi.getPluginName());
+    brokerApi.get().receiveAsync(topic, consumer);
+    if (replayAllEvents) {
+      brokerApi.get().replayAllEvents(topic);
+    }
+  }
+
+  public static class Module extends AbstractModule {
+    @Override
+    protected void configure() {
+      bind(WebSessionBrokerApiLoadedListener.class);
+      bind(StartPluginListener.class)
+          .annotatedWith(UniqueAnnotations.create())
+          .to(WebSessionBrokerApiLoadedListener.class);
+    }
+  }
+}
